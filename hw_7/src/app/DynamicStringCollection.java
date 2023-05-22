10191e86d8c2acb8dfa063e84c62ecbb8e35ca92
Dmytro1988 <yaholnyk88@gmail.com>
2023-05-22 12:38:51 +0300

    'completed'

--- hw_7/src/app/DynamicStringCollection.java
+++ hw_7/src/app/DynamicStringCollection.java
@@ -1,63 +1,87 @@
 package app;
 
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 public class DynamicStringCollection {
-    private final List<String> list;
+    private static final int DEFAULT_CAPACITY = 10;
+    private String[] elements;
+    private int size;
 
     public DynamicStringCollection() {
-        list = new ArrayList<>();
+        elements = new String[DEFAULT_CAPACITY];
+        size = 0;
     }
 
     public boolean add(String value) {
-        list.add(value);
-        System.out.println("Додано до масиву '" + value + "'");
-        return true;
+        return add(size,value);
     }
 
     public boolean add(int index, String value) {
-        if (index >= 0 && index <= list.size()) {
-            list.add(index, value);
-            System.out.println("Додано до масиву '" + value + "' за індесом " + index);
-            return true;
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+          System.out.println("Додано до масиву '" + value + "' за індесом " + index);
+        if (size == elements.length)
+            resizeArray();
+
+        for (int i = size - 1; i >= index; i--) {
+            elements[i + 1] = elements[i];
         }
-        System.out.println("Невірний індекс!");
+        elements[index] = value;
+        size++;
+        return true;
+    }
+
+    public boolean delete(String value) {
+       int index = indexOf(value);
+        if (index != -1) {
+            delete(index);
+            System.out.println("Елемент '" + value + "' видалено з листа");
+            return true;
+        } else {
+            System.out.println("Елемент '" + value + "' не знайдено у листі");
+        }
         return false;
     }
 
     public boolean delete(int index) {
-        if (index >= 0 && index < list.size()) {
-            String deletedElement = list.remove(index);
-            System.out.println("Видалено з масиву '" + deletedElement + "' за індексом " + index);
-            return true;
-        }
-        System.out.println("Невірний індекс!");
-        return false;
-    }
 
-    public boolean delete(String value) {
-        boolean removed = list.remove(value);
-        if (removed) {
-            System.out.println("Елемент '" + value + "' видалено з листа");
-        } else {
-            System.out.println("Елемент '" + value + "' не знайдено у листі");
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        for (int i = index; i < size; i++) {
+            elements[i] = elements[i + 1];
         }
-        return removed;
+        elements[size - 1] = null;
+        size--;
+        System.out.println("Видалено з масиву за індексом " + index);
+        return true;
+
     }
 
     public String get(int index) {
-        if (index >= 0 && index < list.size()) {
-            String element = list.get(index);
-            System.out.println("Елемент за індексом " + index + ": ");
-            return element;
+        if (index < 0 || index > size)
+           throw new IndexOutOfBoundsException();
+        System.out.println("Елемент за індексом " + index + ": ");
+        return elements[index];
+    }
+
+    private void resizeArray(){
+        int newCAPACITY = elements.length * 2;
+        String[] newElements = new String[newCAPACITY];
+        System.arraycopy(elements, 0, newElements, 0, size);
+        elements = newElements;
+    }
+    private  int indexOf(String value){
+        for(int i = 0; i < size; i++){
+            if (value.equals(elements[i])){
+                return i;
+            }
         }
-        System.out.println("Невірний індекс!");
-        return null;
+        return -1;
     }
 
     public void printCollection() {
-        for (String mas : list ) {
+        for (String mas : elements ) {
             System.out.println(mas);
         }
     }
